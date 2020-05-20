34f25fcb40d34c2a6bfc756f2436eb1906bf6969
Shane Neuville <shneuvil@microsoft.com>
2020-05-19 21:37:41 -0600

    update to latest

--- Xamarin.Forms.Template.Against.Source.Android/MainActivity.cs
+++ Xamarin.Forms.Template.Against.Source.Android/MainActivity.cs
@@ -14,12 +14,8 @@ namespace Xamarin.Forms.Template.Against.Source.Droid
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
-
-            global::Xamarin.Forms.Forms.SetFlags("CollectionView_Experimental");
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
